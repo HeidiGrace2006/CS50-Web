9c605b603c6f83374c7de7bcd09ef9dc3814ee37
Heidi Grace <heidi.grace2006@gmail.com>
2024-10-15 09:48:50 -0500

    added app 'tasks'

---
+++ Week3/lecture3/tasks/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
+from django import forms
+from django.urls import reverse
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
+    priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
+
+# Create your views here.
+def index(request):
+    if "tasks" not in request.session: #aka, if user on that session doesn't have a task list yet
+        request.session["tasks"] = []
+    return render(request, "tasks/index.html", {
+        "tasks": request.session["tasks"]
+    })
+
+def add(request):
+    if request.method == "POST":
+        form = NewTaskForm(request.POST) #request.POST contains data that the user submits
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            request.session["tasks"] += [task]
+            task.append(task)
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            return render(request, "tasks/add.html", {
+                "form": form
+            })
+
+    return render(request, "tasks/add.html", {
+        "form": NewTaskForm()
+    })
