f70457e90754d3cc955985dc7841d7a83a4044b8
Heidi Grace <heidi.grace2006@gmail.com>
2024-10-15 19:03:36 -0500

    Finished wiki

--- Week3/wiki/encyclopedia/urls.py
+++ Week3/wiki/encyclopedia/urls.py
@@ -6,5 +6,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path('search/', views.search, name='search'),
+    path('create/', views.create, name="create"),
     path("wiki/<str:entry>/", views.entry, name="entry"),
+    path("random/", views.random_page, name="random"),
 ]
--- Week3/wiki/encyclopedia/views.py
+++ Week3/wiki/encyclopedia/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from . import util
 import markdown
+import random
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -25,6 +26,23 @@ def search(request):
             })
     return redirect(reverse("index"))   
 
+def create(request):
+    if request.method == "POST":
+        title = request.POST.get("title")
+        content = request.Post.get("content")
+        
+        if title in util.list_entries():
+            return render(request, "encyclopedia/error.html", {
+            "message": f"Entry with title '{title}' already exists."
+        })
+        
+        else:
+            #save the page as a md file (Title.md) in the folder entries
+            util.save_entry(title, content)
+            return redirect(reverse("entry", kwargs={"entry": title}))
+
+    return render(request, "encyclopedia/create.html")
+
 def entry(request, entry):
     entry_content = util.get_entry(entry)
     if entry_content is None:
@@ -39,3 +57,9 @@ def entry(request, entry):
         "entry": entry_content
     })
 
+def random_page(request):
+    entries = util.list_entries()
+
+    random_page = random.choice(entries)
+
+    return redirect(reverse("entry", kwargs={"entry": random_page}))
